52ee3d2946168fdd6bdd222fa04a016b1cb05e68
Ngoga Innocent Patrick <ngogainnocent1@gmail.com>
2024-12-17 10:13:28 +0200

    successful

--- Payments/signals.py
+++ Payments/signals.py
@@ -5,12 +5,13 @@ from Notifications.send_Push_Notification import send_push_notification
 @receiver(post_save,sender=Payment)
 def update_subscription(instance,sender,created,**kwargs):
     if not created:
+        print("signal instance", instance.payment_status,instance.subscription_type)
         if instance.payment_status == 'completed' and instance.subscription_type !='Donation':
             try:
                 find_user_subscription=Subscriptions.objects.get(device=instance.device_tokem)
                 find_user_subscription.subscription_type=instance.subscription_type
                 find_user_subscription.active=True
-                if instance.subscription=='by_number':
+                if instance.subscription_type=='by_number':
                     copies=round(instance.amount /20)
                     find_user_subscription.amount_of_copies = find_user_subscription.amount_of_copies + copies
 
@@ -22,14 +23,21 @@ def update_subscription(instance,sender,created,**kwargs):
                     copies=round(instance.amount /20)
                     subscription.amount_of_copies = subscription.amount_of_copies + copies
                     subscription.save()
-        elif instance.payment_status =='completed' and instance.subscription_type =='Donation':
+        elif instance.payment_status =='successful' and instance.subscription_type =='Donation':
             try:
                 token = instance.device_tokem
                 if token !='Donating_Device':
-                    send_push_notification(token, "Thank you for a donation", "We have received your Donation worthy May God support your Daily activities"+str(instance.amount))
+                    send_push_notification(token, "Thank you for The donation", "We have received your Donation worthy May God support your Daily activities")
             except Exception as e:
                 print(e)
                 # logger.error(f"Failed to send notification to device {token}: {e}")
+    # else:
+    #     try:
+    #         token = instance.device_tokem
+    #         if token !='Donating_Device':
+    #             send_push_notification(token, "Thank you for a donation", "We have received your Donation worthy May God support your Daily activities")
+    #     except Exception as e:
+    #             print(e)                    
                             
                 
         
